e605ad91a13e09b04d2edc2672d639b67bc9c313
Sabrina Andrew <sabrina.rae.andrew@gmail.com>
2019-01-29 20:02:42 -0500

    First Commit

---
+++ MyAgeAndFriendsAge/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MyAgeAndFriendsAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myAge;
+            string myName;
+            int friendsAge;
+            string friendsName;
+            
+            Console.WriteLine("How old are you?");
+            myAge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What is your name?");
+            myName = Console.ReadLine();
+            Console.WriteLine("Your name is " + myName + " and you are" + myAge + " years old!");
+
+            Console.WriteLine("How old is your friend?");
+            friendsAge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What is your friends name?");
+            friendsName = Console.ReadLine();
+            Console.WriteLine("Your friends name is " + friendsName + "and they are " + friendsAge + " years old!");
+
+            int oldPeople = myAge + friendsAge;
+
+            Console.WriteLine("You ages added together is " + oldPeople + " years!");
+
+            
+        }
+    }
+}
